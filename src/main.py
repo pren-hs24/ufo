1d3d06c14a1c2e0b78d9fce2f293267e428293f0
David Dias Horta <david.diashorta@stud.hslu.ch>
2025-02-28 15:16:48 +0100

    [PREN2-24] use args instead of hardcoded values

--- src/main.py
+++ src/main.py
@@ -6,22 +6,38 @@ __copyright__ = "Copyright (c) 2025 HSLU PREN Team 2, FS25. All rights reserved.
 import logging
 import logging.config
 import asyncio
+from argparse import ArgumentParser, Namespace
 
-import uvloop  # evdev
+import uvloop
 
 from common.application import log_configuration
 from uart.bus import UARTBus
 from uart.sender import UARTSender
 
 
-async def async_main() -> None:
+def _get_args(logger: logging.Logger) -> Namespace:
+    """Parse command line arguments."""
+    parser = ArgumentParser(description="PREN project FS25 HSLU Team 2")
+    parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+    parser.add_argument(
+        "--bus", type=str, default="/dev/ttyUSB0", help="UART bus device"
+    )
+    parser.add_argument(
+        "--baudrate", type=int, default=115200, help="UART bus baudrate"
+    )
+    args = parser.parse_args()
+    logger.debug("args: %s", args)
+    return args
+
+
+async def async_main(args: Namespace) -> None:
     """Main async function."""
-    reader, writer = await asyncio.open_connection("/dev/ttyUSB0", 115200)
+    reader, writer = await asyncio.open_connection(args.bus, args.baudrate)
     uart = UARTBus(reader, writer)
     sender = UARTSender(uart)
     # receiver = UARTReceiver(uart)
 
-    await sender.set_debug_logging(True)
+    await sender.set_debug_logging(args.debug)
     await sender.turn(90)
     await sender.set_speed(50)
     await sender.set_speed(0)
@@ -32,10 +48,11 @@ def main() -> None:
     logging.config.dictConfig(log_configuration())
     logger = logging.getLogger(__name__)
     logger.info("[main]")
+    args = _get_args(logger)
 
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-    asyncio.run(async_main())
+    asyncio.run(async_main(args))
 
     logger.info("[main] exit")
     logging.shutdown()
