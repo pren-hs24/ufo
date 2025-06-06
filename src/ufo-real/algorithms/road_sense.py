2dc144b05795fb9c66fd1b849e0780f5d1c6fe31
dxstiny <dxstiny.gh@gmail.com>
2025-06-06 17:55:43 +0200

    real: ignore nextPointBlocked if moving

--- src/ufo-real/algorithms/road_sense.py
+++ src/ufo-real/algorithms/road_sense.py
@@ -3,6 +3,8 @@
 
 __copyright__ = "Copyright (c) 2025 HSLU PREN Team 2, FS25. All rights reserved."
 
+from logging import getLogger
+
 from pathfinder.ipathfinder import IPathfinder
 from pathfinder.dijkstra import DijkstraPathfinder
 from uart.receiver import UARTReceiver
@@ -12,7 +14,7 @@ from network.node import Node
 from .base_algorithm import BaseAlgorithm
 
 
-class RoadSenseAlgorithm(BaseAlgorithm):
+class RoadSenseAlgorithm(BaseAlgorithm):  # pylint: disable=too-many-instance-attributes
     """RoadSense"""
 
     def __init__(
@@ -27,6 +29,8 @@ class RoadSenseAlgorithm(BaseAlgorithm):
         self._target: Node | None = None
         self._recalculation_required = False
         self._has_started = False
+        self._is_moving = False
+        self._logger = getLogger("RoadSenseAlgorithm")
 
     def _set_new_path(self) -> None:
         assert self._target is not None
@@ -42,8 +46,11 @@ class RoadSenseAlgorithm(BaseAlgorithm):
     async def _on_start(self, target: Node) -> None:
         self._target = target
         await self._ufo.follow_to_next_node()  # to start
+        self._is_moving = True
 
     async def _on_point_reached(self) -> None:
+        self._is_moving = False
+
         if not self._has_started:
             self._has_started = True
             await self._restart()
@@ -66,8 +73,11 @@ class RoadSenseAlgorithm(BaseAlgorithm):
         )
 
     async def _on_next_point_blocked(self) -> None:
-        self._path[self._node_index + 1].disabled = True
         self._ufo.on_next_node_blocked()
+        if self._is_moving:
+            self._logger.debug("Next point blocked, handle after returning")
+            return
+        self._path[self._node_index + 1].disabled = True
         await self._restart()
 
     async def _on_no_line_found(self) -> None:
@@ -84,6 +94,7 @@ class RoadSenseAlgorithm(BaseAlgorithm):
         if hold:
             return
         await self._ufo.follow_to_next_node()
+        self._is_moving = True
 
     @property
     def name(self) -> str:
