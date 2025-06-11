a46bba01d20a9f9b4fb73a37427f541b3d8c81f3
dxstiny <dxstiny.gh@gmail.com>
2025-06-11 19:15:36 +0200

    real: allow updating network

--- src/ufo-real/web/handlers/system.py
+++ src/ufo-real/web/handlers/system.py
@@ -3,9 +3,14 @@
 
 __copyright__ = "Copyright (c) 2025 HSLU PREN Team 2, FS25. All rights reserved."
 
+import dataclasses
+
 from aiohttp import web
+from pyaddict.schema import Object, Float
 
 from common.constants import VERSION
+from common.competition import update_dynamic_network, create_dynamic_network
+from network.node import NodeLabel
 from algorithms import ALGORITHMS
 from .base_handler import BaseHandler
 
@@ -18,7 +23,9 @@ class SystemHandler(BaseHandler):
         app.router.add_get("/api/system/algorithm", self._algorithm)
         app.router.add_put("/api/system/algorithm", self._set_algorithm)
         app.router.add_get("/api/system/algorithms", self._algorithms)
-        app.router.add_post("/api/system/algorithm/reset", self.reset)
+        app.router.add_post("/api/system/algorithm/reset", self._reset)
+        app.router.add_get("/api/system/network", self._get_network)
+        app.router.add_put("/api/system/network", self._set_network)
 
     async def _version(self, _: web.Request) -> web.Response:
         return web.Response(text=VERSION)
@@ -46,7 +53,29 @@ class SystemHandler(BaseHandler):
     async def _algorithms(self, _: web.Request) -> web.Response:
         return web.json_response(list(ALGORITHMS.keys()))
 
-    async def reset(self, _: web.Request) -> web.Response:
+    async def _reset(self, _: web.Request) -> web.Response:
         """Reset the system"""
         self._engine.reset()
         return web.Response(text="Engine reset")
+
+    async def _get_network(self, _: web.Request) -> web.Response:
+        """Get the current network configuration"""
+        network = create_dynamic_network()
+        data = [dataclasses.asdict(x) for x in network.nodes]
+        return web.json_response(data)
+
+    async def _set_network(self, request: web.Request) -> web.Response:
+        """Set the network configuration"""
+        location_schema = Object(
+            {
+                "x": Float().coerce(),
+                "y": Float().coerce(),
+            }
+        )
+        schema = Object({x.value: location_schema
+                         for x in NodeLabel
+                         if x != NodeLabel.UNDEFINED})
+
+        data = await self._get_json_body(request, schema)
+        update_dynamic_network(data)
+        return web.Response()
