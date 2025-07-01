1cf966e71ae72cb6fc27667b5bac290da0173be3
Anthony Wernli <71585688+anthony-we-ch@users.noreply.github.com>
2025-07-01 18:36:18 +0200

    Testing pushes and uploads (#7)
    * Testing pushes and uploads
    
    * Trial Run um zu sehen ob mypy online funktioniert
    
    * An schlüsselstellen den check disabled damit der merge funktioniert.
    
    ---------
    
    Co-authored-by: anthony.wernli <anthony.wernli@stud.hslu.ch>

---
+++ src/ufo-real/stub/uvloop.pyi
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""UART uvloop"""
+
+__copyright__ = "Copyright (c) 2025 HSLU PREN Team 2, FS25. All rights reserved."
+
+class UvLoop:
+    """stub uvloop for mypy"""
+    def __init__(self) -> None: ...
+    @classmethod
+    def EventLoopPolicy(cls) -> None:  # pylint: disable=invalid-name
+        """
+        This will only return ``None``, please double check your
+        imports if you see this message. It means you are using
+        a stub instead of the real thing.
+        """
